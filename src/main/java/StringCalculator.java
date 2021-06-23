84eb05ed91a485a74df98ba5fb1ac6dd91b96103
Ruiting Zhang <ruiting.zhang@bellevuecollege.edu>
2021-06-23 12:57:19 -0700

    initial

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,13 @@
+public class StringCalculator {
+    public static int add(final String numbers){
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(",|\n");
+        for(String number : numbersArray){
+            if(!number.trim().isempty()){
+                returnValue += Integer.parseInt(number.trim());
+            }
+        }
+
+        return returnValue;
+    }
+}
